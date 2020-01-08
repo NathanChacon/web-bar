01d199164d4af850183f78d5249087a4baee467c
Nathan Chacon <nathanchacon321@gmail.com>
2020-01-07 22:26:16 -0300

    Updates

---
+++ precache-manifest.79c0bfdcb0a0039f0bf9ea9d6e44db87.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46522e3240fb2c605380a642c2d56d3a",
+    "url": "/web-bar/index.html"
+  },
+  {
+    "revision": "64701533615503bc8b71",
+    "url": "/web-bar/static/css/main.0bb621b1.chunk.css"
+  },
+  {
+    "revision": "96b176a9b229ecb15248",
+    "url": "/web-bar/static/js/2.d8ebd338.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/web-bar/static/js/2.d8ebd338.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64701533615503bc8b71",
+    "url": "/web-bar/static/js/main.69da0588.chunk.js"
+  },
+  {
+    "revision": "15780df52891663a3680",
+    "url": "/web-bar/static/js/runtime-main.7ce28cb6.js"
+  },
+  {
+    "revision": "627fb0c3d625bff72d9d6fc0fae2221f",
+    "url": "/web-bar/static/media/barrels.627fb0c3.jpeg"
+  },
+  {
+    "revision": "ba8684594416a15973568f0faa4d2904",
+    "url": "/web-bar/static/media/drinks.ba868459.jpg"
+  },
+  {
+    "revision": "1bce34374f78a104be716cfb36619691",
+    "url": "/web-bar/static/media/homeBackground.1bce3437.jpg"
+  },
+  {
+    "revision": "3b49ac035c3b3cb22062677874f6cadf",
+    "url": "/web-bar/static/media/menu.3b49ac03.jpg"
+  }
+]);
